963adee3274d97d1c9277020b85d2d1c9010cbb4
Trevor Broaddus <trevor.broaddus@gmail.com>
2020-07-16 18:08:35 -0500

    Initialize repo with the PoC gist stimulus component

---
+++ gist/stimulus_component.rb
@@ -0,0 +1,29 @@
+class StimulusComponent < BaseComponent
+  include Stimulus::Dsl
+
+  def initialize(controller:, actions: [])
+    @controller = controller
+    @actions = actions
+  end
+
+private
+
+  attr_reader :controller, :actions
+
+  def action_attributes
+    action_string = ""
+    actions.each do |action_details|
+      action_string += action(action_details[:event_type], action_details[:controller], action_details[:controller_method])
+    end
+
+    action_string
+  end
+
+  def controller_id
+    controller.underscore
+  end
+
+  def controller_class
+    "#{controller.camelize}"
+  end
+end
---
+++ gist/stimulus_dsl.rb
@@ -0,0 +1,20 @@
+module Stimulus
+  module Dsl
+    def initialize_controller
+      "data-controller=#{controller}"
+    end
+
+    def action(event_type, controller, controller_method)
+
+      "data-action=#{event_type}->#{controller.to_s.underscore}##{controller_method}"
+    end
+
+    def data_target(controller, property)
+      "data-target=#{controller}.#{property}"
+    end
+
+    def data_attribute(attribute, value)
+      "data-content-#{dasherize(attribute)}=#{value}"
+    end
+  end
+end
